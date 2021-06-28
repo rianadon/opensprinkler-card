9c689386573c34f34d1d1654c34a6e960935c927
Ryan Adolf <ryanadolf123@gmail.com>
2021-06-27 20:15:59 -0700

    Loosen entity id checks
    Fixes #9. Looks good testing locally, so I'm crossing my fingers I
    don't have to revert this change.

--- src/helpers.ts
+++ src/helpers.ts
@@ -11,17 +11,18 @@ const STOPPABLE_STATES = [...ACTIVE_STATES, ...WAITING_STATES]
 
 export const isStation    = (entity: HassEntity) =>
     entity.attributes.opensprinkler_type === 'station' &&
-    entity.entity_id.startsWith('sensor.') && entity.entity_id.endsWith('_status');
+    entity.entity_id.startsWith('sensor.');
 export const isProgram    = (entity: HassEntity) =>
     entity.attributes.opensprinkler_type === 'program' &&
-    entity.entity_id.startsWith('binary_sensor.') && entity.entity_id.endsWith('_running');
+    entity.entity_id.startsWith('binary_sensor.')
 export const isController = (entity: HassEntity) =>
     entity.attributes.opensprinkler_type === 'controller' &&
-    entity.entity_id.startsWith('switch.') && entity.entity_id.endsWith('_enabled');
+    entity.entity_id.startsWith('switch.');
+
 export const isRunOnce = (entity: HassEntity) => entity.entity_id === 'run_once';
 export const isState = (entity: HassEntity) => !entity.attributes?.opensprinkler_type;
 export const isStationProgEnable = (entity: HassEntity) =>
-    entity.entity_id.startsWith('switch.') && entity.entity_id.endsWith('_enabled');
+    entity.entity_id.startsWith('switch.');
 
 export function hasRunOnce(entities: EntitiesFunc) {
     return entities(isStation).some(e => e.attributes.running_program_id === RUN_ONCE_ID);
